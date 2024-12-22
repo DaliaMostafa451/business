bdc7567349476e49d4da39d23c770e37ebac7b73
Dalia_Mostafa <dalymost0@gmail.com>
2024-12-22 22:12:01 +0000

    n

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(MyHomePage());
 }
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+  // const MyHomePage({Key? key}) : super(key: key);
+  // make a image in my app
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF01addf),
+        body: Center(
+          child: Image(
+            // to make color of back ground of image make it the color of background of bag
+            image: AssetImage(
+              'images/cat.jpg',
+            ),
+          ),
         ),
       ),
     );
